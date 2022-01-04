1feb5441a9aeb4951ed6847a8f024455020dbd5b
Bouddha-ctrl <miri.mohamed.1998@hotmail.com>
2022-01-04 02:02:58 +0100

    GUI v2, remain : mise en forme

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const test = 2   ;
 
 ////////// GLOBAL FUNCTION
 function avg(column){
-    return column.reduce((a,b)=>(a+b),0)/column.length;
+    return Math.round(column.reduce((a,b)=>(a+b),0)/column.length* 100) / 100;
 }
 
 function tableAvg(table){  //rot, arr, rendement 
@@ -88,11 +88,8 @@ function fifo(taches,numberPross){
         const element = array[0];
         array.shift();  
 
-        
-        
-        console.log("befor",output);
         output = output.concat(CreateTache([element]));
-        console.log("after",output);
+        
         let h = Math.max(currPosition[currPross],element.start);
         currPosition[currPross]=h+element.duration;
 
@@ -112,7 +109,7 @@ function fifo(taches,numberPross){
 
     }
     table = createTable(output);    
-    output = output.sort( (a, b)=> (a.tache > b.tache)? 1:-1); //p1 -> p2 -> p3
+    output = output.sort( (a, b)=> (a.name > b.name)? 1:-1); //p1 -> p2 -> p3
 
     return [output,table]
 }
@@ -132,7 +129,7 @@ function removeObject(array,obj){
 function SJF(taches,numberPross){
     
     console.log("SJF function");
-    let array = taches.sort((a, b) =>( a.start > b.start) ? 1 : ((a.name > b.name)? 1:-1 ) );
+    let Open = taches.sort((a, b) =>( a.start > b.start) ? 1 : ((a.name > b.name)? 1:-1 ) );
 
     let Close = []
     let output =[];
@@ -146,7 +143,7 @@ function SJF(taches,numberPross){
         currPross = currPosition.indexOf(Math.min(...currPosition));
 
         let help = Open.filter((a)=>(a.start <= currPosition[currPross] && !Close.includes(a)));
-
+        
         const element = help.length == 0 ?  Open[0]:ShortDuration(help) ; 
 
         Open = removeObject(Open,element);
@@ -171,6 +168,7 @@ function SJF(taches,numberPross){
 
     table = createTable(output);
 
+    output = output.sort( (a, b)=> (a.name > b.name)? 1:-1); //p1 -> p2 -> p3
     return [output,table]
 }
 /////////////////////////////////////////
@@ -182,13 +180,13 @@ function SJF(taches,numberPross){
 function rr(taches, q, numberPross){
     
     console.log("rr function");
-    let array = taches.sort((a, b) =>( a.start > b.start) ? 1 : ((a.name > b.name)? 1:-1 ) );
+    let array = taches;
     
     let output =[];
     let table= []   //rot, attent, rendement
 
     let Open = []  //[t1, t2, t3]
-    let currTache = 0;
+    let currTache = -1;
 
     let currPosition = new Array(numberPross).fill(0);  //currPosition of every pross
     let currPross = 0;
@@ -199,7 +197,8 @@ function rr(taches, q, numberPross){
         if (array.length != 0){
             
             let selected = array.filter((a)=>(a.start <= currPosition[currPross] ));
-
+            selected = selected.sort((a, b) =>( a.start > b.start) ? 1 : ((a.name > b.name)? 1:-1 ) );//p1 -> p2 -> p3
+            
             if (selected.length==0 && Open.length==0){
                 
                 selected = [array[0]];
@@ -212,8 +211,7 @@ function rr(taches, q, numberPross){
         }
 
         
-        currTache++;
-
+        currTache++;    
         currTache = currTache%Open.length;
         const element = Open[currTache];
         
@@ -247,6 +245,7 @@ function rr(taches, q, numberPross){
     }
     table = createTable(output);
     
+    output = output.sort( (a, b)=> (a.name > b.name)? 1:-1); //p1 -> p2 -> p3
     return [output,table]
 }
 /////////////////////////////////////////
@@ -355,7 +354,6 @@ function addTache(btn){
         myOutput = array[0];
         myTable = array[1];
         showTableResult(myTable);
-        console.log(myOutput);
         render(myOutput)
     }
     
@@ -400,13 +398,13 @@ function showTableResult(data){
 
 
 function render(data) {
+
     //header
     let colors = new Map()
     let maxIndex = Math.max(...data.reduce((output, e) => output = [...output, ...e.quantum.map(quantam => +quantam.end)], []))
     let parts = Math.ceil(maxIndex / 5)
     document.querySelector('.gantt').innerHTML = `<div class=header><div class="peace">0</div>`
-    for (let i = 1; i <= parts; i++) document.querySelector('.gantt .header').innerHTML += `<div class="peace" style="width:${5 * 30}px;">${i * 5}</div>`
-
+    for (let i = 1; i <= parts; i++) document.querySelector('.gantt .header').innerHTML += `<div class="peaceHeade" style="width:${5 * 30}px;">${i * 5}</div>`
     data.forEach((elm) => {
         //name
         const div = document.createElement('div')
@@ -418,22 +416,38 @@ function render(data) {
         div.appendChild(span)
         document.querySelector('.gantt').appendChild(div)
         //lmachakil
-        elm.quantum.forEach(e => {
-            let color = '#fff'
-            for (let i = 0; i < 5 * parts; i++) {
-                if (i >= e.start && i < e.end) {
-                    if (!colors.has(e.pross)) {
-                        color = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
-                        colors.set(e.pross, color)
-                    } else color = colors.get(e.pross)
-                }
+        elmentIndexs = elm.quantum.map(quantam => +quantam.end)
+        for (let i = 0; i < 5 * parts; i++) {
+            let e = elm.quantum.find(quantam => i >= quantam.start && i < quantam.end)
+            if (e) {
+                let color = '#fff'
+                if (!colors.has(e.pross)) {
+                    color = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+                    colors.set(e.pross, color)
+                } else color = colors.get(e.pross)
                 const span = document.createElement('div')
                 span.setAttribute('class', "Element")
                 span.setAttribute('class', "peace")
                 span.style.backgroundColor = color
                 div.appendChild(span)
                 color = '#fff'
+            } else {
+                const span = document.createElement('div')
+                span.setAttribute('class', "Element")
+                span.setAttribute('class', "peace")
+                span.style.backgroundColor = "#fff"
+                div.appendChild(span)
             }
-        })
+        }
+
     })
+
+    window.addEventListener('resize', () => {
+        document.querySelectorAll('.peace').forEach(e => e.style.width = `${window.innerWidth / (parts * 5 + 2)}px`)
+        document.querySelectorAll('.peace').forEach(e => e.style.height = `${window.innerWidth / (parts * 5 + 2)}px`)
+        document.querySelectorAll('.peaceHeade').forEach(e => e.style.width = `${(window.innerWidth / (parts * 5 + 2)) * 5}px`)
+    });
+    document.querySelectorAll('.peace').forEach(e => e.style.width = `${window.innerWidth / (parts * 5 + 2)}px`)
+        document.querySelectorAll('.peace').forEach(e => e.style.height = `${window.innerWidth / (parts * 5 + 2)}px`)
+        document.querySelectorAll('.peaceHeade').forEach(e => e.style.width = `${(window.innerWidth / (parts * 5 + 2)) * 5}px`)
 }
